07e3748e5893d0f15127a3058a4a053034af8328
PauloAragaodeAzevedoNeto <92046894+PauloAragaodeAzevedoNeto@users.noreply.github.com>
2022-04-01 14:29:25 -0300

    MeusDados

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import HelloWorld from "./components/HelloWorld";
+import MeusDados from "./components/MeusDados";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
       <HelloWorld />
+      <MeusDados />
     </div>
   );
 }
---
+++ src/components/MeusDados.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+const MeusDados = () => (
+  <div>
+    <h3>Nome: Paulo</h3>
+    <h3>Curso: RC</h3>
+    <h3>Universidade: UFC - Quixadá</h3>
+  </div>
+);
+export default MeusDados;
