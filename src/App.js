fedf9452ccaf43d9df6c4b18a87412dfe39f47b0
PauloAragaodeAzevedoNeto <92046894+PauloAragaodeAzevedoNeto@users.noreply.github.com>
2022-04-01 15:37:35 -0300

    IMCclasse

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import HelloWorld from "./components/HelloWorld";
+import IMC from "./components/IMC/IMC";
 import MeusDados from "./components/MeusDados";
 import MeusDadosProps from "./components/MeusDadosProps";
 import Recursividade from "./components/Recursividade";
@@ -7,7 +8,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <Recursividade />
+      <IMC peso="80.00" altura="1.70" />
     </div>
   );
 }
